4115724b7baab3261234fa76d40d23c5bc7da46b
Zarapwa <doulatshahiinfo@gmail.com>
2025-12-06 11:46:25 +0330

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
-self.addEventListener("fetch", (event) => {
-  // همیشه از شبکه بخوان، هیچ‌وقت از cache نده
-  event.respondWith(fetch(event.request));
-});
